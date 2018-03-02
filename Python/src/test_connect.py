b0885aefed4e7ff630d432346aa75269d723afa0
Royran <ruoyi_ran@foxmail.com>
2018-03-02 18:00:48 +0800

    add test python script

---
+++ Python/src/test_connect.py
@@ -0,0 +1,9 @@
+'''
+@version: 1.0
+@author: royran
+@contact: iranpeng@gmail.com
+@file: test_connect.py
+@time: 2018/2/28 18:15
+'''
+from unity_environment import UnityEnvironment
+env = UnityEnvironment()
