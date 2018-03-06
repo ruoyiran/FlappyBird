247231d1566a1a839bdeadec8944f4559d4bf99e
Roy Ran <ruoyi_ran@foxmail.com>
2018-03-07 03:42:30 +0800

    add ENABLE_TENSORFLOW macro

--- Assets/Scripts/PlayMode/AIPlayMode.cs
+++ Assets/Scripts/PlayMode/AIPlayMode.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.IO;
+#if ENABLE_TENSORFLOW
 using TensorFlow;
+#endif
 using UnityEngine;
 
 namespace FlappyBird
 {
     public class AIPlayMode : BasePlayMode, IPlayMode
     {
+#if ENABLE_TENSORFLOW
         private TFModel _tfTrainingModel;
         private TFModel _tfPreprocessingModel;
         private const string TF_TRAINING_MODEL_PATH = "TFModel/graph_def.bytes";
@@ -18,11 +21,14 @@ namespace FlappyBird
         private const string NET_INPUT_NAME = "MainQNetwork/Input/x";
         private const string NET_OUTPUT_NAME = "MainQNetwork/Qout/QValue";
         private TFTensor inputTensor = null;
+#endif
 
         private void Awake()
         {
+#if ENABLE_TENSORFLOW
             _tfTrainingModel = new TFModel();
             _tfPreprocessingModel = new TFModel();
+#endif
         }
 
         private void Start()
@@ -36,6 +42,7 @@ namespace FlappyBird
 
         private void FixedUpdate()
         {
+#if ENABLE_TENSORFLOW
             if (_isPlaying && _tfTrainingModel.IsLoaded && _tfPreprocessingModel.IsLoaded)
             {
                 if (GameManager.Instance.frameRecorder.IsCaptured)
@@ -44,10 +51,12 @@ namespace FlappyBird
                     GameManager.Instance.bird.Flap(action);
                 }
             }
+#endif
         }
 
         private Bird.Action GetAction()
         {
+#if ENABLE_TENSORFLOW
             byte[] imageData = GameManager.Instance.frameRecorder.GetFrameImageData();
             if (inputTensor == null)
             {
@@ -66,14 +75,19 @@ namespace FlappyBird
             float q1 = qvalues[0, 0];
             float q2 = qvalues[0, 1];
             return q2 > q1 ? Bird.Action.Flap : Bird.Action.Idle;
+#else
+            return Bird.Action.Idle;
+#endif
         }
 
         public new void Play()
         {
+#if ENABLE_TENSORFLOW
             inputTensor = null;
             GameManager.Instance.frameRecorder.BeginRecording();
             LoadTFTrainingModel();
             LoadTFPreprocessingGraph();
+#endif
             base.Play();
         }
 
@@ -85,22 +99,30 @@ namespace FlappyBird
 
         private bool LoadTFTrainingModel()
         {
+#if ENABLE_TENSORFLOW
             if (_tfTrainingModel == null || _tfTrainingModel.IsLoaded)
                 return false;
             if (File.Exists(Path.Combine(Application.persistentDataPath, TF_TRAINING_MODEL_PATH)))
                 return _tfTrainingModel.LoadModel(Path.Combine(Application.persistentDataPath, TF_TRAINING_MODEL_PATH));
             else
                 return _tfTrainingModel.LoadModel(Path.Combine(Application.streamingAssetsPath, TF_TRAINING_MODEL_PATH));
+#else
+            return false;
+#endif
         }
 
         private bool LoadTFPreprocessingGraph()
         {
+#if ENABLE_TENSORFLOW
             if (_tfPreprocessingModel == null || _tfPreprocessingModel.IsLoaded)
                 return false;
             if (File.Exists(Path.Combine(Application.persistentDataPath, TF_PREPROCESSING_GRAPH_DEF_PATH)))
                 return _tfPreprocessingModel.LoadGraphDef(Path.Combine(Application.persistentDataPath, TF_PREPROCESSING_GRAPH_DEF_PATH));
             else
                 return _tfPreprocessingModel.LoadGraphDef(Path.Combine(Application.streamingAssetsPath, TF_PREPROCESSING_GRAPH_DEF_PATH));
+#else
+            return false;
+#endif
         }
     }
 }
